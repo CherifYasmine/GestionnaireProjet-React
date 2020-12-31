fb15768e32dd8f939d747e3d8d69d3f64807022e
CherifYasmine <yasmine.cherif9@gmail.com>
2020-12-31 16:44:49 +0100

    Home and Navbar are responsive now

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,33 +1,35 @@
 import React from "react";
 import "./Footer.css";
 import FontAwesome from "react-fontawesome";
-
+import { Container, Row, Col } from "react-bootstrap";
 const Footer = () => {
   return (
-    <div className="footer copyright py-4 text-center ">
-      <hr />
-
-      <small className="foot">
-        Copyright © Clazana {new Date().getFullYear()}
-      </small>
-      <a className="terms" href="/ ">
-        Terms of use
-      </a>
-      <div className="icons">
-        <a href="/">
-          <FontAwesome className="facebook" name="facebook" />
-        </a>
-        <a href="/">
-          <FontAwesome className="linkedin" name="linkedin" />
-        </a>
-        <a href="/">
-          <FontAwesome className="github" name="github" />
-        </a>
-        <a href="/">
-          <FontAwesome className="twitter" name="twitter" />
-        </a>
-      </div>
-    </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col>copyright &copy; Clazana {new Date().getFullYear()}</Col>
+          <Col>
+            <a href="/ ">Terms of use</a>
+          </Col>
+          <Col>
+            <div>
+              <a href="/">
+                <FontAwesome name="facebook" />
+              </a>
+              <a href="/">
+                <FontAwesome name="linkedin" />
+              </a>
+              <a href="/">
+                <FontAwesome name="github" />
+              </a>
+              <a href="/">
+                <FontAwesome name="twitter" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
